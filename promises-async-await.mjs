25e6f9ef9a2e33d729e7906143cb3d21c32a3aae
Martin Burolla <mburolla@xpanxion.com>
2022-03-21 11:09:57 -0400

    update

--- promises-async-await.mjs
+++ promises-async-await.mjs
@@ -3,13 +3,31 @@
 // Date: 3/21/2022
 // 
 
-const main = () => {
-    console.log('here')
+const myPromise = () => {
+    return new Promise(function(resolve, reject) {
+        if (true) {
+            resolve("Stuff worked.");
+        }
+        else {
+            reject("It broke!");
+        }
+  }) 
+}
+
+const main1 = () => {  // Old way, not preferred.
+    myPromise()
+        .then(r => console.log(r))
+        .catch(r =>console.log(r)); 
+}
+
+const main2 = async() => { // Elegant way, preferred.
+    let r = await myPromise();
+    console.log(r);
 }
 
 //////////////////////////////////////////////////////////////////////////
 // Topics
 /////////////////////////////////////////////////////////////////////////
 
-main();
-
+// main1();
+main2();
