2ce25069651d4e42abc9a60e91f4b357d13ee22e
Martin Burolla <mburolla@xpanxion.com>
2022-03-21 09:08:44 -0400

    update

--- readme.md
+++ readme.md
@@ -13,8 +13,12 @@
 
 # Notes
 - Functions are first class citizens in JavaScript
-- JavaScript has [two types](https://www.javascripttutorial.net/javascript-primitive-vs-reference-values/#:~:text=Primitive%20values%20(null%2C%20undefined%2C,values%20that%20refer%20to%20objects.): 
+- JavaScript has [two types](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures): 
   - Primitive types: (null, undefined, boolean, number, string, symbol, and BigInt)
   - Reference types: (objects)
 
 - [Coercion (casting)](https://www.freecodecamp.org/news/js-type-coercion-explained-27ba3d9a2839/)
+- [Hoisting])(https://www.w3schools.com/js/js_hoisting.asp)
+- [Null vs Undefined](https://flexiple.com/undefined-vs-null-javascript/#:~:text=Unassigned%20variables%20are%20initialized%20by%20JavaScript%20with%20a%20default%20value%20of%20undefined.&text=Here%20as%20the%20variable%20is,a%20representation%20of%20no%20value.)
+- [Symbols](https://www.programiz.com/javascript/symbol)
+- [Memory Stack vs Heap](https://felixgerschau.com/javascript-memory-management/)
--- types-coercion-equality.js
+++ types-coercion-equality.js
@@ -3,11 +3,61 @@
 // Date: 3/21/2022
 //
 
-
 const types = () => {
     // (null, undefined, boolean, number, string, symbol, and BigInt)
-    let b = false;
+    let b = false; // JavaScript infers the type for us: boolean.
+    // Boolean b = false; // Don't have to do this in JavaScript because its a loosely typed dynamic language.
+
     console.log(typeof(b));
+    b = 123; // Now JavaScript infers the type for us: number.
+    console.log(typeof(b));
+}
+
+const typesPrimative = () => {
+    //
+    // Primative types (Stack):
+    //  null, undefined, boolean, number, string, symbol, and BigInt
+    //
+    let a = null;
+    let b = undefined;
+    let c = false; 
+    let d = 123;
+    let e = "Hello World";
+    let f = Symbol('foo');
+    let g = 2n ** 53n;
+    
+    console.log(typeof(a)); // <== object???
+    console.log(typeof(b));
+    console.log(typeof(c));
+    console.log(typeof(d));
+    console.log(typeof(e));
+    console.log(typeof(f));
+    console.log(typeof(g));
+
+    console.log(f.description);
+
+    // These are the important ones: null, undefined, boolean, number, string
+}
+
+const typesReference = () => {
+    // Reference types point to objects (Heap).
+
+    // Array
+    let messyArray = [1, 2, 3, 'test', 1.234, null, "test"];
+    console.log(messyArray);
+    console.log(typeof(messyArray));
+    console.log(typeof(messyArray[3])); // Arrays are zero based. 
+
+    // Dictionary
+    let dictionary = {};
+    dictionary[1] = 'one';
+    dictionary[2] = 'two';
+    dictionary[3] = 'three';
+    console.log(dictionary);
+    console.log(typeof(dictionary));
+
+    // 
+
 }
 
 const coercion = () => {
@@ -24,6 +74,8 @@ const equality = () => {
 // Topics
 /////////////////////////////////////////////////////////////////////////
 
-types();
+//types();
+//typesPrimative();
+typesReference();
 // coercion();
 // equality();
