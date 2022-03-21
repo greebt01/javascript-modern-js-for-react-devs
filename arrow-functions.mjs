796e600b0cec4fb337fe5da44f6e6118f49f7b60
Martin Burolla <mburolla@xpanxion.com>
2022-03-21 09:54:41 -0400

    update

--- arrow-functions.mjs
+++ arrow-functions.mjs
@@ -3,12 +3,20 @@
 // Date: 3/21/2022
 //
 
-function oldFunc() { // <== Don't get used to this format.
+function oldFunc1() { // <== Don't get used to this format.
     console.log("I am old.");
 }
 
-const newFunc = () => { // This is an anonymous function.
+const newFunc1 = () => { // This is an anonymous function.
     console.log("I am new.")
+}
+
+function oldFunc2(name) { // <== Don't get used to this format.
+    console.log(`My name is ${name}.`);
+}
+
+const newFunc2 = (name) => { // This is an anonymous function.
+    console.log(`My name is ${name}.`)
 }
 
 
@@ -17,7 +25,8 @@ const newFunc = () => { // This is an anonymous function.
 // Topics
 /////////////////////////////////////////////////////////////////////////
 
-oldFunc();
-newFunc();
-
+oldFunc1();
+newFunc1();
+oldFunc2("Mary");
+newFunc2("Mary");
 
