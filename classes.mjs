c2e10c56b15894a8adb41be5d953f9ee04b79b9b
Martin Burolla <mburolla@xpanxion.com>
2022-03-21 10:39:43 -0400

    update

--- classes.mjs
+++ classes.mjs
@@ -8,15 +8,18 @@ import Person from "./Person.mjs";
 
 const test = () => {
 
-    let p1 = new Person('Joe');
+    let p1 = new Person('Joe'); // Instaniate a Person.
     p1.introduceSelf();
 
-    let c1 = new Cat("Gypsy");
+    let c1 = new Cat("Gypsy"); // Instaniate a Cat.
     // let c2 = new Cat("Gypsy", "black"); // Constructor overloading is not supported in JavaScript.
     //console.log(c1.age) // Private!  Returns undefined.
-
     console.log(c1.speak2());
     console.log(c1.speak3());
+
+    // Pass a Cat into a Person.
+    p1.setCat(c1);
+    console.log(`The name of my cat is: ${p1.getCatName()}.`);
 }
 
 //////////////////////////////////////////////////////////////////////////
