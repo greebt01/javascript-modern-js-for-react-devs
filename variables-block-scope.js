8e49b3a6c73940ee8b42cf23ddab78af51095f41
Martin Burolla <mburolla@xpanxion.com>
2022-05-24 10:35:43 -0400

    update

--- variables-block-scope.js
+++ variables-block-scope.js
@@ -30,7 +30,7 @@ const constants = () => {
 
 const scope = () => {
     // let and const have block scope, between the curly braces.
-    const MEANING_OF_LIFE = 5150; // <== this is used, not the one on line 6.
+    const MEANING_OF_LIFE = 42; // <== this is used, not the one on line 6.
     let num1 = 70; // <== this is used, not the ones on line 9, 16.
     var num2 = 54; // <== Old ES 5 approach, DO NOT USE VAR.
 
@@ -47,7 +47,7 @@ const scope = () => {
 // Topics
 /////////////////////////////////////////////////////////////////////////
 
-//variables();
+variables();
 //constants();
-scope();
+//scope();
 
