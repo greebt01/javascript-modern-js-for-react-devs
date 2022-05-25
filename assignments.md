adbe891e49a6e948d3b9fd136764499880f1beeb
Martin Burolla <mburolla@xpanxion.com>
2022-05-24 20:31:49 -0400

    update

--- assignments.md
+++ assignments.md
@@ -31,8 +31,34 @@ Output:
 ```
 
 ## Ex. 3
+Create an arrow function called `interleave` that accepts two arrays and interleave them.  The function returns the following error string if the two arrays have a different length:  `ERROR: Array length mismatch`
+
+Usage #1:
+```
+const array1 = [1, 2, 3, 4, 5];
+const array2 = ['a', 'b', 'c', 'd', 'e'];
+console.log(interleave(array1, array2));
+```
+
+Output:
+```
+1a2b3c4d5e
+```
 
 
+Usage #2:
+```
+const array1 = [1];
+const array2 = ['a', 'b', 'c', 'd', 'e'];
+console.log(interleave(array1, array2));
+```
+
+Output:
+```
+ERROR: Array length mismatch
+```
+
+## Ex. 4
 
 
 
--- solutions/solutions2.js
+++ solutions/solutions2.js
@@ -14,7 +14,9 @@ const ex2 = () => {
 }
 
 const ex3 = () => {
-    
+    const array1 = [1, 2, 3, 4, 5];
+    const array2 = ['a', 'b', 'c', 'd', 'e'];
+    console.log(interleave(array1, array2));
 }
 
 
@@ -42,8 +44,20 @@ const countNumbers = (array) => {
     return retval;
 }
 
+const interleave = (array1, array2) => {
+    let retval = 'ERROR: Array length mismatch';
+    if (array1.length == array2.length) {
+        retval = '';
+        for (let i = 0; i < array1.length; i++) {
+            retval += array1[i] + array2[i];
+        }
+    }
+    return retval;
+}
+
+
 const main = async () => {
-    ex2();
+    ex3();
 }
 
 main();
